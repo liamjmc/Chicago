2cb9a269cd1dac5874fec9e3c593e3310779fd77
Liam McCann <liam.mccann@geeks.ltd.uk>
2014-07-29 09:12:45 +0100

    Initial commit containing files written in first session.

---
+++ chicago-app-controller.js
@@ -0,0 +1,8 @@
+/// <reference path="//ajax.googleapis.com/ajax/libs/angularjs/1.2.21/angular.min.js" />
+// JavaScript source code
+
+var ChicagoApp = angular.module('chicagoApp', []);
+
+ChicagoApp.controller('chicacoController', ['$scope', function($scope) {
+
+}]);
---
+++ chicago-directives.js
@@ -0,0 +1,14 @@
+/// <reference path="//ajax.googleapis.com/ajax/libs/angularjs/1.2.21/angular.min.js" />
+
+angular.module('ChicacoDirectives', [])
+
+    .directive('ball', function () {
+        return {
+            restrict: 'E',
+            template:
+            el.attr
+
+
+        }
+
+    })
